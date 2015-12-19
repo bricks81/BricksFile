403a1955346c954c0af236f8e131cbf071ea6769
Sven Ullmann <sven.ullmann@e3n.de>
2015-12-19 22:30:45 +0100

    make things easier

--- src/Bricks/File/DirectoryInterface.php
+++ src/Bricks/File/DirectoryInterface.php
@@ -27,72 +27,21 @@
 
 namespace Bricks\File;
 
-use Bricks\File\File;
-
 /**
  * Class extending SPL class and adding diffrent directory operations
  */
-class Directory extends \RecursiveDirectoryIterator 
-implements DirectoryInterface {
+interface DirectoryInterface  extends \RecursiveDirectoryIterator {
 
 	/**
 	 * @param string $dir
 	 * @param integer $mode
 	 * @return \Bricks\File\Directory
 	 */
-	public static function mkdir($dir,$mode=0750){
-		mkdir($dir,$mode,true);
-		return new self($dir);
-	}
+	public static function mkdir($dir,$mode=0750);
 	
 	/**
 	 * @param string $dir
 	 */
-	public static function rmdir($dir){
-		if($dir instanceof Directory){
-			$dir = $dir->getPathname();
-		}
- 		$dh = opendir($dir);
- 		while(false!==($file=readdir($dh))){
- 			if('.'==$file||'..'==$file){
- 				continue;
- 			}
- 			if(is_dir($dir.'/'.$file)){
- 				self::rmdir($dir.'/'.$file);
- 			} else {
- 				File::unlink($dir.'/'.$file);
- 			}
- 		}
- 		closedir($dh);
- 		rmdir($dir);
- 	}
- 	
- 	/**
- 	 * @param string $target
- 	 * @param integer $mode
- 	 * @param integer $fmode
- 	 * @return \Bricks\File\Directory
- 	 */
- 	public function copy($target,$mode=0750,$fmode=0644){
- 		if(!file_exists($target)){ 			
- 			mkdir($target,$mode,true);
- 		}
- 		$dir = $this->getPathname();
- 		$dh = opendir($dir);
- 		while(false!==($file=readdir($dh))){
- 			if('.'==$file||'..'==$file){
- 				continue;
- 			}
- 			if(is_dir($dir.'/'.$file)){
- 				$_d = new self($dir.'/'.$file);
- 				$_d->copy($target.'/'.$file);
- 			} else {
- 				copy($dir.'/'.$file,$target.'/'.$file);
- 				chmod($dir.'/'.$file,$fmode); 				 				
- 			}
- 		}
- 		closedir($dh);
- 		return $this;
- 	}
+	public static function rmdir($dir);
 	
 }
--- src/Bricks/File/FileInterface.php
+++ src/Bricks/File/FileInterface.php
@@ -27,12 +27,9 @@
 
 namespace Bricks\File;
 
-use JMS\Serializer\Exception\RuntimeException;
-
 use Bricks\File\Directory;
 
-class File extends \SplFileObject 
-implements FileInterface {
+interface FileInterface extends \SplFileObject {
 
 	
 	/**
@@ -41,25 +38,14 @@ implements FileInterface {
 	 * @param integer $dmode
 	 * @return \Bricks\File\File
 	 */
-	public static function touch($file,$mode=0644,$dmode=0750){
-		if(!file_exists(dirname($file))){
-			Directory::mkdir(dirname($file),$dmode);			
-		}
-		touch($file);
-		chmod($file,$mode);
-		return new self($file);		
-	}
+	public static function touch($file,$mode=0644,$dmode=0750);
 	
 	/**
 	 * @param string $source
 	 * @param string $target
 	 * @return \Bricks\File\File
 	 */
-	public static function staticCopy($source,$target){
-		$file = new self($source);
-		$file->copy($target);
-		return new self($target);
-	}
+	public static function staticCopy($source,$target);
 	
 	/**
 	 * @param string $target
@@ -67,24 +53,12 @@ implements FileInterface {
 	 * @param integer $dmode
 	 * @return \Bricks\File\File
 	 */
-	public function copy($target,$mode=0644,$dmode=0750){
-		$source = $this->getPathname();
-		if(!file_exists(dirname($target))){
-			Directory::mkdir(dirname($target),$dmode);
-		}
-		copy($source,$target);
-		return new self($target);
-	}
+	public function copy($target,$mode=0644,$dmode=0750);
 	
 	/**
 	 * @param string|File $file
 	 */
-	public static function unlink($file){
-		if($file instanceof File){
-			$file = $file->getPathname();
-		}
-		unlink($file);			
-	}
+	public static function unlink($file);
 	
 	/**
 	 * Cleans a filename and replace special characters
@@ -92,34 +66,13 @@ implements FileInterface {
 	 * @param string $replacement
 	 * @return string
 	 */
-	public static function cleanFilename($filename,$replacement='-'){
-		$filename = str_replace("\0",'',$filename);
-		$filename = preg_replace('#[^a-zA-Z0-9._-]+#b',$replacement,$filename);
-		if(strlen($filename)>255){
-			$ext = substr($filename,strrpos($filename,'.'));
-			$name = substr($filename,0,255-strlen($ext));
-			$filename = $name.$ext;
-		}
-		return $filename;
-	}
+	public static function cleanFilename($filename,$replacement='-');
 	
 	/**
 	 * Adds or increment a prefixed integer in the path
 	 * @param string $path
 	 * @return string
 	 */
-	public static function incrementFilename($path){
-		$i = 1;
-		$dir = dirname($path);
-		$filename = basename($path);
-		if(preg_match('#^(\d+)-(.*)$#',basename($path),$matches)){
-			$i = (int) $matches[1];
-			$filename = $matches[2];
-		}
-		do {
-			$target = rtrim($dir,'/').'/'.self::cleanFilename($i++.'-'.$filename);
-		} while(file_exists($target));
-		return $target;
-	}
+	public static function incrementFilename($path);
 	
 }
